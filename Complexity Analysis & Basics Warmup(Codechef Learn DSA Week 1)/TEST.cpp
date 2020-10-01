38215242a0183a2cdd28317b2e45281eec4f525f
aditya576 <55613768+aditya576@users.noreply.github.com>
2020-10-01 21:58:41 +0530

    Update TEST.cpp

--- Complexity Analysis & Basics Warmup(Codechef Learn DSA Week 1)/TEST.cpp
+++ Complexity Analysis & Basics Warmup(Codechef Learn DSA Week 1)/TEST.cpp
@@ -1,17 +1,18 @@
 //https://www.codechef.com/LRNDSA01/problems/TEST
+//This is a problem for newbies
 #include <iostream>
 using namespace std;
 
 int main() {
 	// your code goes here
-	while(1)
+	while(1)//This loop will run until the loop encounters a break statement
 	{
 	    int n;
-	    std::cin>>n;
-	    if(n==42)
+	    std::cin>>n;//takes input of n
+	    if(n==42)// if it gets an input of n=42,it will come out of the loop and it will not take any more input
 	    break;
 	    
-	    std::cout<<n<<'\n';
+	    std::cout<<n<<'\n';//prints the value of n, '\n' inserts a new line
 	}
 	return 0;
 }
