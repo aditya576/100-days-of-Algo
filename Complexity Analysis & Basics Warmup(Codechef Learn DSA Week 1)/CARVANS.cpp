99068d289c931876e8b454cb1fe800ed91429d3d
aditya576 <55613768+aditya576@users.noreply.github.com>
2020-10-01 20:49:52 +0530

    Update CARVANS.cpp

--- Complexity Analysis & Basics Warmup(Codechef Learn DSA Week 1)/CARVANS.cpp
+++ Complexity Analysis & Basics Warmup(Codechef Learn DSA Week 1)/CARVANS.cpp
@@ -5,21 +5,21 @@ using namespace std;
 int main() {
 	int t;
 	scanf("%d",&t);
-	while(t--)
+	while(t--)//while t is greater than 0,this loop will run
 	{
 	    int n,a[10000];
 	    std::cin>>n;//8 3 6
-	    int max=n;
+	    int max=n;//Maximum value is the size of the array now,considering that every car is moving at its maximum speed 
 	    for(int i=0;i<n;i++)
 	    std::cin>>a[i];
 	    for(int i=1;i<n;i++)
 	    {
 	        for(int j=0;j<i;j++)
 	        {
-	            if(a[i]>a[j])
+	            if(a[i]>a[j])//if any jth car moving before the ith car is moving at a lower speed , then ith car is not moving at its maximum speed
 	            {
-	                max--;
-	                break;
+	                max--;//maximum value will be one less now
+	                break;//it will come out of the 2nd loop
 	            }
 	        }
 	    }
